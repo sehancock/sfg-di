9969786678c2abd5b302ea0dde4c3d602a7e6ac1
Scott Hancock <scottehancock@gmail.com>
2020-12-05 14:00:33 -0500

    34. Dependency Injection without Spring

---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControlledTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControlledTest {
+
+    SetterInjectedControlled controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedControlled();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
